26e63cf837e96ddfce5a5995f7d13b1d201d71e5
Nolan Wallace <NolanrWallace03@gmail.com>
2020-07-08 14:10:35 -0700

    first commit(done)

---
+++ src/Components/HelloDojo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class HelloDojo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Dojo!</h1>
+            </div>
+        )
+    }
+}
+
+
